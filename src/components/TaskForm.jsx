94759fc15523fc1de9bd9b60e1178585952f463d
samuel-villa <ciullasamuel@gmail.com>
2024-04-19 16:23:48 +0200

    [IMP] todo list dynamic

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,34 +1,54 @@
-export function TodoItem() {
-  return (
-    <li></li>
-  );
-}
-
-
-export function TodoList() {
-  return (
-    <ul>
-      <li>
-        <input type="checkbox" /> Item 1
-      </li>
-      <li>
-        <input type="checkbox" /> Item 2
-      </li>
-    </ul>
-  );
-}
+import React, { useState } from "react";
 
 
 export function TaskForm() {
-    return (
-      <section className="formSection">
-        <form action="submit">
-            <label htmlFor="new-task">Enter New Task</label>
-            <input id="new-task" name="new-task" type="text" />
-            <input id="submitBtn" type="submit" value="Add ToDo" />
-        </form>
-        <TodoList />
-      </section>
-    );
-  }
+
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== '') {
+      const newTodo = {
+        id: todos.length - 1,
+        title: inputValue,
+        done: false
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  return (
+    <section className="formSection">
+      <form id="itemsForm" onSubmit={handleAddTodo}>
+        <label htmlFor="new-task">Enter New Task</label>
+        <input
+          id="new-task"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
