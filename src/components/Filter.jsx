088e2c2c646abb1be41300b4e03350a0996ec2c0
samuel-villa <ciullasamuel@gmail.com>
2024-04-23 14:59:42 +0200

    [ADD] filter style

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,6 +7,8 @@ export function Filter({ setFilter }) {
 
     return (
         <div className="filter">
+            <h2>3</h2>
+            <hr></hr>
             <button className="filter-btn" data-filter="all" onClick={handleFilterClick}>All</button>
             <button className="filter-btn" data-filter="done" onClick={handleFilterClick}>Done</button>
             <button className="filter-btn" data-filter="todo" onClick={handleFilterClick}>To Do</button>
